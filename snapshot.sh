e3711e9ac6f9bf67a2237183404554fc792e49e1
sumitkumarjethani <sumitkumar_jethani@hotmail.com>
2021-01-20 21:01:42 +0000

    Final Commit

--- snapshot.sh
+++ snapshot.sh
@@ -14,6 +14,16 @@ error() {
 	exit 1
 }
 
+# Comprobación del fichero de configuración en caso que se
+# tome por defecto y no este situado en la ruta esperada
+check_configFile() {
+	if [[ ! -f $config_file ]]
+	then
+		error "Fichero de configuración por defecto no existe en el lugar de por defecto: /root/Monitorizacion_de_ficheros/config_file"
+	fi
+}
+
+
 # Función de limpieza del directorio /Snapshot
 clean() {
 	find "$directory" -mindepth 1 -maxdepth 1 -delete
@@ -35,6 +45,7 @@ then
 	else
 		echo "Fichero de configuración no existe: tomado el de por defecto"
 		config_file="/root/Monitorizacion_de_ficheros/config_file"
+		check_configFile
 	fi
 elif [[ $# -eq 1 ]]
 then
@@ -42,6 +53,7 @@ then
 	then
 		clean
 		config_file="/root/Monitorizacion_de_ficheros/config_file"
+		check_configFile
 	elif [[ -f $1 ]]
 	then
 		config_file=$1
@@ -49,7 +61,9 @@ then
 else
 	echo "Fichero de configuración: tomado el de por defecto"
 	config_file="/root/Monitorizacion_de_ficheros/config_file"
+	check_configFile
 fi
+
 
 # Creación del directorio que contendra la "foto"
 
